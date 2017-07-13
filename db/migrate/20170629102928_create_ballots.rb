adeb68f6d025e24f2555d49149232297b7ed011e
Kari Matthews <karimatthews3@gmail.com>
2017-07-13 16:11:12 +1000

    remove migration

--- db/migrate/20170629102928_create_ballots.rb
+++ db/migrate/20170629102928_create_ballots.rb
@@ -3,7 +3,6 @@ class CreateBallots < ActiveRecord::Migration[5.1]
     create_table :ballots do |t|
       t.references :poll, foreign_key: true
       t.string :name
-
       t.timestamps
     end
   end
--- db/migrate/20170630110232_add_name_to_ballot.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToBallot < ActiveRecord::Migration[5.1]
-  def change
-    add_column :ballots, :name, :string
-  end
-end
